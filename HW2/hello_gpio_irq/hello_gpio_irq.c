b03068be156a21eed8c2823281bd0befb18efe83
Zach <zachharding34@gmail.com>
2025-04-07 17:35:41 -0500

    hw 2 done

---
+++ HW2/hello_gpio_irq/hello_gpio_irq.c
@@ -0,0 +1,105 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+#define GPIO_WATCH_PIN 2
+
+#ifdef CYW43_WL_GPIO_LED_PIN
+#include "pico/cyw43_arch.h"
+#endif
+
+#define LED_PIN 16
+
+static char event_str[128];
+int num_pressed = 0;
+
+void gpio_event_string(char *buf, uint32_t events);
+
+// Perform initialisation
+int pico_led_init(void) {
+    #if defined(LED_PIN)
+        // A device like Pico that uses a GPIO for the LED will define PICO_DEFAULT_LED_PIN
+        // so we can use normal GPIO functionality to turn the led on and off
+        gpio_init(LED_PIN);
+        gpio_set_dir(LED_PIN, GPIO_OUT);
+        return PICO_OK;
+    #elif defined(CYW43_WL_GPIO_LED_PIN)
+        // For Pico W devices we need to initialise the driver etc
+        return cyw43_arch_init();
+    #endif
+    }
+    
+    // Turn the led on or off
+    void pico_set_led(bool led_on) {
+    #if defined(LED_PIN)
+        // Just set the GPIO on or off
+        gpio_put(LED_PIN, led_on);
+    #elif defined(CYW43_WL_GPIO_LED_PIN)
+        // Ask the wifi "driver" to set the GPIO on or off
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
+    #endif
+    }
+    
+
+void gpio_callback(uint gpio, uint32_t events) {
+    // Put the GPIO event(s) that just happened into event_str
+    // so we can print it
+    gpio_event_string(event_str, events);
+    //printf("GPIO %d %s\n", gpio, event_str);
+    num_pressed++;
+    printf("Number of button presses: %d\n", num_pressed);
+    pico_set_led(true);
+    sleep_ms(200);
+    pico_set_led(false);
+}
+
+
+int main() {
+    stdio_init_all();
+    printf("Hello GPIO IRQ\n");
+    gpio_init(GPIO_WATCH_PIN);
+    int rc = pico_led_init();
+    hard_assert(rc == PICO_OK);
+    while(true){
+        gpio_set_irq_enabled_with_callback(GPIO_WATCH_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
+
+        //gpio_set_irq_enabled_with_callback(GPIO_WATCH_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+    }
+    
+    
+}
+
+
+static const char *gpio_irq_str[] = {
+        "LEVEL_LOW",  // 0x1
+        "LEVEL_HIGH", // 0x2
+        "EDGE_FALL",  // 0x4
+        "EDGE_RISE"   // 0x8
+};
+
+void gpio_event_string(char *buf, uint32_t events) {
+    for (uint i = 0; i < 4; i++) {
+        uint mask = (1 << i);
+        if (events & mask) {
+            // Copy this event string into the user string
+            const char *event_str = gpio_irq_str[i];
+            while (*event_str != '\0') {
+                *buf++ = *event_str++;
+            }
+            events &= ~mask;
+
+            // If more events add ", "
+            if (events) {
+                *buf++ = ',';
+                *buf++ = ' ';
+            }
+        }
+    }
+    *buf++ = '\0';
+}

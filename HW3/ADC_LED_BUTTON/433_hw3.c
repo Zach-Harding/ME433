4ff918c42cc06f3c4d835e4f3135ce4c42cec29f
Zach <zachharding34@gmail.com>
2025-04-09 12:11:41 -0500

    HW3 complete

--- HW3/ADC_LED_BUTTON/433_hw3.c
+++ HW3/ADC_LED_BUTTON/433_hw3.c
@@ -10,7 +10,7 @@
 #include "hardware/adc.h"
 
 #define GPIO_WATCH_PIN 2
-#define LED_PIN 25 // GPIO pin for the LED
+#define LED_PIN 16 // GPIO pin for the LED
 
 static char event_str[128];
 
@@ -44,40 +44,57 @@ void gpio_callback(uint gpio, uint32_t events) {
     // Put the GPIO event(s) that just happened into event_str
     // so we can print it
     gpio_event_string(event_str, events);
-    printf("GPIO %d %s\n", gpio, event_str);
+    //printf("GPIO %d %s\n", gpio, event_str);
 }
 
-adc_init(); // init the adc module
-adc_gpio_init(26); // set ADC0 pin to be adc input instead of GPIO
-adc_select_input(0); // select to read from ADC0
+
 
 int main() {
     stdio_init_all();
+    pico_led_init();
+    adc_init(); // init the adc module
+    adc_gpio_init(26); // set ADC0 pin to be adc input instead of GPIO
+    adc_select_input(0); // select to read from ADC0
+
     while (!stdio_usb_connected()) {  // waits until the USB port has been opened
         sleep_ms(100);
     }
     printf("Start!\n");
+    printf("ADC initial value: %d\n", adc_read()); 
 
-    //gpio_init(GPIO_WATCH_PIN);
-    //gpio_set_irq_enabled_with_callback(GPIO_WATCH_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+    gpio_init(GPIO_WATCH_PIN);
+    gpio_set_irq_enabled_with_callback(GPIO_WATCH_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
 
     // Infinite loop to keep the program running
     while (1) {
         // TODO:
         // Turns on an LED
-        // Waits until a button is pressed
+        pico_set_led(true);
+        printf("Press the button to continue...\n");
+        while(!gpio_get(GPIO_WATCH_PIN)) { // Waits until a button is pressed
+            sleep_ms(50);
+        }
         // Turns off the LED
+        pico_set_led(false); // Turns off the LED
+        sleep_ms(50); 
+        
 
         // Asks the user to enter a number of analog samples to take, between 1 and 100
         // Reads the number entered by the user
-        char message[100];
-        scanf("%s", message);
-        printf("message: %s\r\n",message);
+        uint16_t i;
+        printf("Enter a Number from 0 to 100: ");
+        scanf("%d", &i);
+        printf("Number: %d\r\n",i);
         sleep_ms(50);
 
         // Reads the voltage on ADC0 that number of times at 100Hz
-        // Prints back the voltages in the units of volts
-        // then loops to ask the user again
+        for(int z = 0; z < i; z++) { 
+            uint16_t result = adc_read(); 
+            float voltage = (float)result * 3.3f / (1 << 12); 
+            printf("ADC0: %f V\r\n", voltage); 
+            sleep_ms(10); // wait for 10ms before taking the next sample
+        }
+        
     }
 }
 
